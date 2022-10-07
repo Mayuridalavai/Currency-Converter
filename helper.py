7eb4eef489147a5221f502f71159655e078e688e
Mayuridalavai <mayuri.gj@gmail.com>
2022-10-07 11:44:02 -0700

    modified errors

--- helper.py
+++ helper.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
 """MDalavai | helper functions for Currency Converter"""
 
-import requests
 import json
 import pprint
+import requests
 
-#function to list all currencies 
+#function to list all currencies
 def list_currencies():
-   
     """List of Currencies"""
-    
     url = "https://api.fastforex.io/currencies?api_key=67f42a4e33-1fcc5e9d1e-rj7nis"
-    data = requests.get(url)
+    data = requests.get(url, timeout=10)
     result = data.json()
     for i in result["currencies"]:
         print(i, end="-------")
         pprint.pprint(result["currencies"][i])
-    print("---------------------------------------------------------")    
+    print("---------------------------------------------------------")
     print(f"Total Number of Currencies : {len(result['currencies'])}")
     print("---------------------------------------------------------")
-     
+
 #function exchange rates
 def exchange_rate(currency1, currency2):
     """Exchange rates"""
@@ -30,11 +28,10 @@ def exchange_rate(currency1, currency2):
     	"X-RapidAPI-Key": "f28ba617dcmshd66f7ecf9ae61c6p17dd07jsne9a5c2bce5ed",
     	"X-RapidAPI-Host": "currency-converter-pro1.p.rapidapi.com"
     }
-    response = requests.request("GET", url, headers=headers, params=querystring)
+    response = requests.request("GET", url, headers=headers, params=querystring, timeout=10)
     data = json.loads(response.text)
     if len(data) == 0:
-       print('Invalid currencies.')
-       return
-
+        print('Invalid currencies.')
+        return
     rate = data.get("result").get(currency2)
-    print(f"{currency1} -> {currency2} = {rate}")
+    print(f"Exchange Rate : {currency1} -> {currency2} = {rate}")
--- project.py
+++ project.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 """MDalavai | Currency Converter"""
 
-import requests
-import pyfiglet
-import json
-from helper import exchange_rate, list_currencies
 from tkinter import *
 from tkinter import Tk, ttk
+import json
 import time
-
-
-
+import requests
+import pyfiglet
+from helper import exchange_rate, list_currencies
 
 # Currency converting and tkinter windohwh
 def convert_window():
@@ -23,33 +20,25 @@ def convert_window():
 
 
     #creating the window
-    window = Tk()
-    window.title('converter')
-    window.configure(bg=cor0)
-    window.geometry('400x420')
-    window.resizable(height= FALSE, width=FALSE)
+    root = Tk()
+    root.title('converter')
+    root.configure(bg=cor0)
+    root.geometry('400x420')
+    root.resizable(height= FALSE, width=FALSE)
 
     #Frames
-    top = Frame(window, width=400, height=60, bg=cor2)
+    top = Frame(root, width=400, height=60, bg=cor2)
     top.grid(row=0,column=0)
-    
-    main_frame = Frame(window, width=400, height=260, bg=cor0)
+    main_frame = Frame(root, width=400, height=260, bg=cor0)
     main_frame.grid(row=1,column=0)
-    
-    
     #create a function convert amount
     def convert():
-        
         """Currency is converter in this function"""
-        
         url = "https://currency-converter18.p.rapidapi.com/api/v1/convert"
-
         currency1 = combo1.get()
         currency2 = combo2.get()
         amount = value.get()
-
         querystring = {"from":currency1,"to":currency2,"amount":amount}
-        
         if currency2 == 'USD':
             symbol = '$'
         elif currency2 == 'INR':
@@ -59,62 +48,54 @@ def convert_window():
         elif currency2 == 'BRL':
             symbol = 'R$'
         elif currency2 == 'CAD':
-            symbol = 'CA$'           
-    
+            symbol = 'CA$'
         headers = {
 	    "X-RapidAPI-Key": "f28ba617dcmshd66f7ecf9ae61c6p17dd07jsne9a5c2bce5ed",
 	    "X-RapidAPI-Host": "currency-converter18.p.rapidapi.com"
-        }    
-        response = requests.request("GET", url, headers=headers, params=querystring)
+        }
+        response = requests.request("GET", url, headers=headers, params=querystring, timeout=10)
         data = json.loads(response.text)
         convert_amt = data["result"]["convertedAmount"]
-        format_amt = symbol + "{:.2f}".format(convert_amt)
+        format_amt = symbol + "{:,.2f}".format(convert_amt)
         result['text'] = format_amt
-        
         print(f"Converted : {convert_amt}, {format_amt}")
 
-    app_name = Label(top, text = 'Currency Converter', anchor="center", font=('Arial 16 bold'), bg=cor2, fg=cor0)
+    app_name = Label(top, text = 'Currency Converter', anchor="center",
+    font=('Arial 16 bold'), bg=cor2, fg=cor0)
     app_name.place(relx ='0.25', rely = '0.2')
-    
     #main frame
-    result = Label(main_frame, text = "", width=18, height=2, pady=7, relief="solid", anchor=CENTER, font=('Ivy 15 bold'), bg=cor0, fg=cor1)
+    result = Label(main_frame, text = "", width=18, height=2, pady=7, relief="solid",anchor=CENTER, font=('Ivy 15 bold'), bg=cor0, fg=cor1)
     result.place(x=90, y=10)
-    
     currency_list  = ['CAD', 'BRL', 'EUR', 'INR','USD']
-    
     #from label
     from_label = Label(main_frame, text = "From", width=8, height=1, pady=0, padx=0, relief="flat", anchor=NW, font=('Ivy 15 bold'), bg=cor0, fg=cor1)
     from_label.place(x=48,y=90)
     combo1 = ttk.Combobox(main_frame, width=8, justify=CENTER, font=("Ivy 12 bold"))
     combo1['values'] = (currency_list)
     combo1.place(x=50, y=115)
-    
     #to label
     to_label = Label(main_frame, text = "To", width=8, height=1, pady=0, padx=0, relief="flat", anchor=NW, font=('Ivy 15 bold'), bg=cor0, fg=cor1)
     to_label.place(x=250,y=90)
     combo2 = ttk.Combobox(main_frame, width=8, justify=CENTER, font=("Ivy 12 bold"))
     combo2['values'] = (currency_list)
     combo2.place(x=250, y=115)
-    
     #for enter the amount
     value = Entry(main_frame, width=30,justify=CENTER, font=("Ivy 12 bold"), relief=SOLID)
     value.place(x=60, y=170)
-    
-    #to click converter button 
+    #to click converter button
     button = Button(main_frame, text="Converter", width=19, height=1, padx=5, font=('Ivy 12 bold'), bg=cor2, fg=cor0, command=convert)
     button.place(x=95, y=220)
-    
-    window.mainloop()
-    
+    root.mainloop()
+
 # main function
 def main():
-    """Main function to get the user inputs"""
+    """Main function to get the user selected inputs"""
     welcome = pyfiglet.figlet_format("Curency Converter", font = "bubble")
     print(welcome,flush=True)
     time.sleep(2)
     user_name = input("Enter your Name --> ")
     time.sleep(2)
-    print(f"Hello {user_name}! Welcome to the Currency Converter..\n", end='',flush=True)
+    print(f"Hello {user_name}! Welcome to the Currency Converter..\n")
     time.sleep(2)
 
     while True:
@@ -135,15 +116,10 @@ def main():
             currency1 = input("Enter a base currency: ").upper()
             currency2 = input("Enter a currency to convert to: ").upper()
             exchange_rate(currency1, currency2)
-
         else:
             print("Sorry! You have not choosen a Valid input")
-
-
     end_messeage = pyfiglet.figlet_format("Thank you! for using Currency Converter", font = "digital")
     print(end_messeage)
 
-
-        
 if __name__ == "__main__":
-    main()    
+    main()
